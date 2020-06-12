82848cc7c87394d9fe436aeac356edb9715ad3da
abibouba <38095792+abibouba@users.noreply.github.com>
2020-06-12 15:05:05 +0200

    Package pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from setuptools import setup, find_packages
-
-VERSION = "0.1"
-
-REQUIRES = ["requests >= 2.23.0", "beautifulsoup4>=4.9.1"]
-
+from distutils.core import setup
 setup(
-    name="swiss-snow",
-    version=VERSION,
-    url="https://github.com/robmarkcole/London-tube-status",
-    author="Sebastien Trosset",
-    author_email="seb@trosset.net",
-    description="Parse Swiss snow conditions from various websites",
-    install_requires=REQUIRES,
-    packages=find_packages(),
-    license="Apache License, Version 2.0",
-    python_requires=">=3.5",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
+  name='swiss-snow',
+  packages=['swiss-snow'],
+  version='0.1',
+  license='Apache License, Version 2.0',
+  description='A Python module for accessing swiss snow conditions and resort status.',
+  author='Seb',
+  author_email='seb@seb.com',
+  url='https://github.com/sebitr/swiss-snow',
+  download_url='https://github.com/sebitr/swiss-snow/archive/v_01.tar.gz',
+  keywords=['swiss', 'snow', 'ski', 'snowboard', 'avalanche'],
+  install_requires=[
+        'requests >= 2.23.0',
+        'beautifulsoup4>=4.9.1',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
